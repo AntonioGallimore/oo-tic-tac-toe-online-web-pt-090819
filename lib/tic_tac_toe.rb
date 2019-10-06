1aefe1113b26fbfa0c1f3376ea052dda8fda2cba
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-06 13:47:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,12 +18,13 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  def WIN_COMBINATIONS
+
     WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5],
   [6,7,8],
   [0,4,8],
-  [2,4,6] ]
+  [2,4,6] 
+  ]
   end
 end

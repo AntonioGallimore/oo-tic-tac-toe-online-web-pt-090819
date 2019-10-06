f09865c5fba170742caa9cd3961f07b7a6d197b5
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-06 14:11:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,9 +29,10 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+  
   def input_to_index(input)
-    input.to_i
-    
-    
+    input.to_i - 1 
+  end
+  def move
   end 
 end
